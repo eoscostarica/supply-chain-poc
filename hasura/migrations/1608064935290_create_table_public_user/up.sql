dc5dfc9dc965f32bedacfb0e86a3961f4b8ef9f9
Adriel Diaz <adriexnet@gmail.com>
2020-12-15 21:21:28 -0600

    feat: add user migration

---
+++ hasura/migrations/1608064935290_create_table_public_user/up.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE TABLE "public"."user"("id" uuid NOT NULL DEFAULT gen_random_uuid(), "name" varchar NOT NULL, "account" varchar, "username" varchar NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, "role" varchar NOT NULL, "email_verified" boolean NOT NULL DEFAULT false, "verification_code" varchar, "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") );
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_user_updated_at"
+BEFORE UPDATE ON "public"."user"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_user_updated_at" ON "public"."user" 
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
