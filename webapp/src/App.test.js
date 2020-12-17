726a103b26dcd13894e7518f57c1435d3c474946
Adriel Diaz <adriexnet@gmail.com>
2020-12-16 21:55:02 -0600

    feat: add new componets for user routes

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ThemeProvider } from '@material-ui/core/styles'
 
 import App from './App'
+import { SharedStateProvider } from './context/state.context'
 import './i18n'
-import * as theme from './theme'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
 
   ReactDOM.render(
-    <ThemeProvider theme={theme.lightTheme}>
+    <SharedStateProvider>
       <App />
-    </ThemeProvider>,
+    </SharedStateProvider>,
     div
   )
   ReactDOM.unmountComponentAtNode(div)
