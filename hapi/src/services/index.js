170ef581d6c0109085779712d0fde58a5af7514e
Adriel Diaz <adriexnet@gmail.com>
2020-12-18 16:02:28 -0600

    feat: add product service

--- hapi/src/services/index.js
+++ hapi/src/services/index.js
@@ -1,9 +1,11 @@
 const manufacturerService = require('./manufacturer.service')
+const productService = require('./product.service')
 const userService = require('./user.service')
 const vaultService = require('./vault.service')
 
 module.exports = {
   userService
   manufacturerService,
+  productService,
   vaultService
 }
