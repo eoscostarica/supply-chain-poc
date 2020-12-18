3fbbce58b7a73e2acfdb83503f1b1ffc77706ce9
Adriel Diaz <adriexnet@gmail.com>
2020-12-18 16:01:25 -0600

    feat: add manufacturer service

--- hapi/src/services/index.js
+++ hapi/src/services/index.js
@@ -1,7 +1,9 @@
+const manufacturerService = require('./manufacturer.service')
 const userService = require('./user.service')
 const vaultService = require('./vault.service')
 
 module.exports = {
   userService
+  manufacturerService,
   vaultService
 }
