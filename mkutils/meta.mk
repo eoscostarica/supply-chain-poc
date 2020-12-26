fbf74188a9874f752bbc4a9dc95bd1716ea94be0
Xavier Fernandez <xavier@crservers.com>
2020-12-26 00:22:32 -0600

    fix(envvar): set hasura URL environmental variable

--- mkutils/meta.mk
+++ mkutils/meta.mk
@@ -1,22 +1,11 @@
 -include .env
 
 VERSION ?= $(shell git rev-parse --short HEAD)
-CURRENT_BRANCH ?= $(git rev-parse --abbrev-ref HEAD)
 NAMESPACE ?= "inmutrust"
-
-ifeq ($(CURRENT_BRANCH),main)
-	REACT_APP_HASURA_URL := https://graphql.inmutrust.com/v1/graphql
-else ifeq ($(CURRENT_BRANCH),dev)
-	REACT_APP_HASURA_URL := https://dev-graphql.inmutrust.com/v1/graphql
-else
-	REACT_APP_HASURA_URL := http://localhost:8585/v1/graphql
-endif
-
 IMAGE_NAME_WEBAPP=inmutrust-webapp
 IMAGE_NAME_HAPI=inmutrust-hapi
 IMAGE_NAME_HASURA=inmutrust-hasura
 IMAGE_NAME_WALLET=inmutrust-wallet
-
 DOCKER_REGISTRY=eoscostarica506
 SUBDIRS=hasura webapp hapi wallet
 
