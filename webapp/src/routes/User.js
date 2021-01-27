4b9bfb05939b938475663df4e3e579ae0b8e6aa8
adriel <adriexnet@gmail.com>
2021-01-27 08:50:56 -0600

    List and view user (#134)
    * feat: add gs1 codes to asset mdata
    
    * fix: codefactor issues
    
    * feat: list and view users

--- webapp/src/routes/User.js
+++ webapp/src/routes/User.js
@@ -1,73 +1,165 @@
-import React, { memo, useState, useEffect } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useLazyQuery } from '@apollo/react-hooks'
-import styled from 'styled-components'
+import Fab from '@material-ui/core/Fab'
+import AddIcon from '@material-ui/icons/Add'
+import { useLazyQuery, useMutation } from '@apollo/react-hooks'
 
+import { useSharedState } from '../context/state.context'
+import MasterDetail from '../layouts/MasterDetail'
+import Loader from '../components/Loader'
+import InfoBox from '../components/InfoBox'
 import ListItems from '../components/ListItems'
-import Tabs from '../components/Tabs'
-import { USER_LIST_QUERY } from '../gql'
-
-const StyledTabs = styled(Tabs)`
-  ${props => props.theme.breakpoints.up('md')} {
-    display: none;
-  }
-`
+import UserInfo from '../components/UserInfo'
+import Modal from '../components/Modal'
+import {
+  USERS_QUERY,
+  USER_QUERY,
+  MANUFACTURER_UPDATE_MUTATION,
+  MANUFACTURER_INSERT_MUTATION
+} from '../gql'
 
 const Users = () => {
-  const [value, setValue] = useState(0)
   const { t } = useTranslation('users')
-  const [getUsers, { data: { users } = {} }] = useLazyQuery(USER_LIST_QUERY)
-
-  const tabs = [
+  const [, setState] = useSharedState()
+  const [selectedId, setSelectedId] = useState()
+  const [currentModal, setCurrentModal] = useState()
+  const [
+    getUsers,
+    { loading: loadingUsers, data: { users } = {} }
+  ] = useLazyQuery(USERS_QUERY, {
+    fetchPolicy: 'network-only'
+  })
+  const [getUser, { loading: loadingUser, data: { user } = {} }] = useLazyQuery(
+    USER_QUERY,
     {
-      label: t('users'),
-      content: (
-        <ListItems
-          items={
-            users
-              ? users.map(({ name, created_at: createAt }) => {
-                  const newDate = new Date(createAt)
-                  const dateFormat = newDate.toLocaleString({
-                    hour: 'numeric',
-                    hour12: true
-                  })
-
-                  return {
-                    title: name,
-                    summary: `Creado el - ${dateFormat}`
-                  }
-                })
-              : []
-          }
-        />
-      )
-    },
-    {
-      label: t('roles'),
-      content: (
-        <ListItems
-          items={[
-            {
-              title: 'Role #1',
-              summary: 'Creado el 12:03:58 12/04/20'
-            },
-            {
-              title: 'Role #2',
-              summary: 'Creado el 12:03:58 12/04/20'
-            }
-          ]}
-        />
-      )
+      fetchPolicy: 'network-only'
     }
-  ]
+  )
+  const [addUser, { loading: addingUser }] = useMutation(
+    MANUFACTURER_INSERT_MUTATION
+  )
+  const [updateUser, { loading: updatingUser }] = useMutation(
+    MANUFACTURER_UPDATE_MUTATION
+  )
 
-  const handleChange = (event, newValue) => setValue(newValue)
+  const handleOnClick = item => {
+    setSelectedId(item.id)
+  }
+
+  const handleOnCloseDetailView = () => {
+    setSelectedId(null)
+  }
+
+  const handleOnSubmit = async ({ id, name, data }) => {
+    if (id) {
+      await handleUpdateManufacturer(id, name, data)
+    } else {
+      await handleAddManufacturer(name, data)
+    }
+
+    setCurrentModal(null)
+  }
+
+  const handleAddManufacturer = async (name, data) => {
+    const { data: response } = await addUser({
+      variables: { name, data }
+    })
+    setState({
+      message: {
+        content: t('successAdd'),
+        type: 'success'
+      }
+    })
+    getUsers()
+    setSelectedId(response.user.id)
+  }
+
+  const handleUpdateManufacturer = async (id, name, data) => {
+    const { data: response } = await updateUser({
+      variables: { id, name, data }
+    })
+    getUser({ variables: { id: response.user.id } })
+    setState({
+      message: {
+        content: t('successUpdate'),
+        type: 'success'
+      }
+    })
+  }
+
+  const renderModalContent = modal => {
+    switch (modal.type) {
+      case 'add':
+        return <p>Work in progress</p>
+      case 'edit':
+        return <p>Work in progress</p>
+
+      default:
+        return <></>
+    }
+  }
 
   useEffect(() => {
     getUsers()
   }, [getUsers])
 
-  return <StyledTabs value={value} onChange={handleChange} items={tabs} />
+  useEffect(() => {
+    if (!selectedId) {
+      return
+    }
+
+    getUser({ variables: { id: selectedId } })
+  }, [selectedId, getUser])
+
+  console.log(handleOnSubmit)
+  console.log(addingUser)
+  console.log(updatingUser)
+  return (
+    <MasterDetail
+      onCloseDetailView={handleOnCloseDetailView}
+      showDetailView={!!selectedId}
+      detailViewTitle={user?.name}
+      detailViewContent={
+        <UserInfo
+          data={user}
+          onClickAction={setCurrentModal}
+          loading={loadingUser}
+        />
+      }
+      actionButton={
+        <Fab
+          color="secondary"
+          aria-label="add"
+          onClick={() => setCurrentModal({ type: 'add' })}
+        >
+          <AddIcon />
+        </Fab>
+      }
+    >
+      {loadingUsers && <Loader />}
+      {!loadingUsers && !users?.length && <InfoBox text={t('emptyMessage')} />}
+      {!loadingUsers && users?.length && (
+        <ListItems
+          items={users.map(item => ({
+            id: item.id,
+            title: item.name,
+            summary: t(item.role)
+          }))}
+          handleOnClick={handleOnClick}
+          selected={selectedId}
+        />
+      )}
+      {currentModal && (
+        <Modal
+          open={!!currentModal}
+          onClose={() => setCurrentModal(null)}
+          title={t(`${currentModal.type}Title`)}
+        >
+          {renderModalContent(currentModal)}
+        </Modal>
+      )}
+    </MasterDetail>
+  )
 }
 
 export default memo(Users)
