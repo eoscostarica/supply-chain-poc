b5628c16b811d4a5db974e219cd1100da261922f
Xavier <5632966+xavier506@users.noreply.github.com>
2021-04-19 09:58:28 -0600

    Migrate Smart Contracts to LAC Chain (#205)
    * Form validations styles (#198)
    
    * fix(FE): form validations styles
    
    * fix(FE): remove console log
    
    * support for lacchain network (#203)
    
    * feat: add optional env variable HAPI_EOS_PROXY_ACTION to can work with custom networks like LACChain
    
    * feat: add support for lachain network
    
    Co-authored-by: Teto Gomez <terencio17@rocketmail.com>
    Co-authored-by: adriel <adriexnet@gmail.com>

--- hapi/src/config/eos.config.js
+++ hapi/src/config/eos.config.js
@@ -3,5 +3,9 @@ module.exports = {
   chainId: process.env.HAPI_EOS_API_CHAIN_ID,
   baseAccount: process.env.HAPI_EOS_BASE_ACCOUNT,
   baseAccountPassword: process.env.HAPI_EOS_BASE_ACCOUNT_PASSWORD,
-  walletUrl: process.env.HAPI_EOS_WALLET_URL
+  authorityForNewAccounts: process.env.HAPI_EOS_AUTHORITY_FOR_NEW_ACCOUNTS,
+  walletUrl: process.env.HAPI_EOS_WALLET_URL,
+  proxyAction: process.env.HAPI_EOS_PROXY_ACTION
+    ? JSON.parse(process.env.HAPI_EOS_PROXY_ACTION)
+    : null
 }
